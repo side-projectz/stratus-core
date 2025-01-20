f641e0d6745df8d2eec33ead42fbf5c5cdb866e1
Surya Umapathy <32648210+suryaumapathy2812@users.noreply.github.com>
2025-01-20 11:35:41 +0530

    minor: workflow response generated with DSPY

--- app/modules/chat/workflow.py
+++ app/modules/chat/workflow.py
@@ -1,8 +1,9 @@
+import dspy
+from dspy import Prediction
 from fastapi.exceptions import HTTPException
-from llama_index.core import QueryBundle, Settings
+from llama_index.core import QueryBundle
 from llama_index.core.postprocessor import SentenceTransformerRerank
 from llama_index.core.response.pprint_utils import pprint_source_node
-from llama_index.core.response_synthesizers import CompactAndRefine
 from llama_index.core.retrievers import VectorIndexRetriever
 from llama_index.core.schema import NodeWithScore
 from llama_index.core.workflow import (
@@ -14,13 +15,17 @@ from llama_index.core.workflow import (
 	step,
 )
 
+from app.config import LLM_MODEL, LLM_PROVIDER, LLM_PROVIDER_API_KEY
 from app.modules.projects import ProjectService
 from app.shared.chroma_db import ChromaDB
 
 
+class RetrieverEvent(Event):
+	query: str
+
+
 class SemanticSearchEvent(Event):
 	query: str
-	collection_name: str
 
 
 class ResponseGenerationEvent(Event):
@@ -30,35 +35,77 @@ class ResponseGenerationEvent(Event):
 
 class RagWorkflow(Workflow):
 	@step
-	async def start_event(self, ctx: Context, e: StartEvent) -> SemanticSearchEvent:
+	async def start_event(self, ctx: Context, e: StartEvent) -> RetrieverEvent:
 		project_id = e.project_id
 		project = ProjectService().get_project(id=project_id)
 		if not project:
 			raise HTTPException(status_code=404, detail="Project not found")
 
-		return SemanticSearchEvent(query=e.query, collection_name=project.name)
+		lm = dspy.LM(
+			model=f"{LLM_PROVIDER}/{LLM_MODEL}",
+			api_key=LLM_PROVIDER_API_KEY,
+		)
+
+		await ctx.set("lm", lm)
+		await ctx.set("collection_name", project.name)
+		await ctx.set("query", e.query)
+
+		return RetrieverEvent(query=e.query)
+
+	@step
+	async def retriever_event(
+		self, ctx: Context, e: RetrieverEvent
+	) -> SemanticSearchEvent:
+		query = e.query
+		collection_name = await ctx.get("collection_name")
+
+		collection_index = ChromaDB(collection_name).as_vector_store_index()
+
+		if collection_index is None:
+			print("Index is empty, load some documents before querying!")
+			return None
+
+		retriever = VectorIndexRetriever(
+			index=collection_index,
+			similarity_top_k=10,
+		)
+
+		query_bundle = QueryBundle(query)
+		retrieved_nodes = await retriever.aretrieve(query_bundle)
+
+		print(f"Retrieved {len(retrieved_nodes)} nodes.")
+		await ctx.set("retrieved_nodes", retrieved_nodes)
+		await ctx.set("query_bundle", query_bundle)
+		return SemanticSearchEvent(query=query)
 
 	@step
 	async def semantic_search(
 		self, ctx: Context, e: SemanticSearchEvent
 	) -> ResponseGenerationEvent:
-		collection_name = e.collection_name
 		query = e.query
+		query_bundle = await ctx.get("query_bundle")
+		retrieved_nodes = await ctx.get("retrieved_nodes")
 
-		collection_index = ChromaDB(collection_name).as_vector_store_index()
-		query_bundle = QueryBundle(query)
-		retriever = VectorIndexRetriever(
-			index=collection_index,
-			similarity_top_k=10,
-		)
-		retrieved_nodes = retriever.retrieve(query_bundle)
 		reranker = SentenceTransformerRerank(
 			model="cross-encoder/ms-marco-MiniLM-L-6-v2",
-			top_n=5,
+			top_n=10,
 		)
+
+		# reranker = LLMRerank(
+		# 	llm=Settings.llm,
+		# 	choice_batch_size=10,
+		# 	top_n=5,
+		# )
+
+		# reranker = RankGPTRerank(
+		# 	llm=Settings.llm,
+		# 	top_n=5,
+		# )
+
 		retrieved_nodes = reranker.postprocess_nodes(retrieved_nodes, query_bundle)
 		for node in retrieved_nodes:
 			pprint_source_node(source_node=node)
+			print("\n")
 
 		return ResponseGenerationEvent(query=query, nodes=retrieved_nodes)
 
@@ -66,11 +113,23 @@ class RagWorkflow(Workflow):
 	async def generate_response(
 		self, ctx: Context, e: ResponseGenerationEvent
 	) -> StopEvent:
-		query = e.query
+		query = await ctx.get("query")
 		retrieved_nodes = e.nodes
-		llm = Settings.llm
 
-		summarizer = CompactAndRefine(llm=llm, streaming=False, verbose=True)
-		response = await summarizer.asynthesize(query, nodes=retrieved_nodes)
+		lm = await ctx.get("lm")
+		dspy.configure(lm=lm)
 
-		return StopEvent(result=str(response))
+		# rag = dspy.ChainOfThought("context, question -> response")
+		rag = dspy.MIPROv2("context, question -> response")
+
+		output = rag(
+			context="\n".join([node.text for node in retrieved_nodes]),
+			question=query,
+		)
+
+		res = {
+			"response": output.response,
+			"reasoning": output.reasoning,
+		}
+
+		return StopEvent(result=res["response"])
